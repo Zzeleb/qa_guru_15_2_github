58a6941ec3bee37bd53f27b1f05c0b6b0ffdde29
Zzeleb <zzeleb@gmail.com>
2022-09-14 17:48:42 +0300

    added second test

--- src/test/java/gmail/zzeleb/ExampleTest.java
+++ src/test/java/gmail/zzeleb/ExampleTest.java
@@ -1,8 +1,19 @@
 package gmail.zzeleb;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 public class ExampleTest {
+
     @Test
-    void exampleTest(){
-       Assertions.assertTrue(2>1);
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
     }
 }
